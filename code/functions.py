a676414d211aac0b9e8126818e7f47421361cf97
jannis <jannis.witte2000@gmail.com>
2026-02-13 01:02:10 +0100

    plot function

---
+++ code/functions.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+def plot(rows, cols, main_title, df_cols):
+    fig, axs = plt.subplots(rows, cols)
+
+    for row in range(rows):
+        for col in range(cols):
+            axs[row, col].hist(df[df_cols[row][col]].values)
+            axs[row, col].set_title("_".join(df_cols[row][col].split("_")[1:]))
+
+    plt.suptitle(main_title)
+    plt.tight_layout()
