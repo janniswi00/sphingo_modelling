f3a8bbcffe4c4f4285e9462f91817896fe7171dc
jannis <jannis.witte2000@gmail.com>
2026-02-17 16:38:50 +0100

    plot function: added bins argument

--- code/functions.py
+++ code/functions.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 
-def plot(rows, cols, main_title, df, df_cols):
+def plot(rows, cols, main_title, df, df_cols, bins):
     fig, axs = plt.subplots(rows, cols)
 
     for row in range(rows):
         for col in range(cols):
-            axs[row, col].hist(df[df_cols[row][col]].values)
+            axs[row, col].hist(df[df_cols[row][col]].values, bins=bins)
             axs[row, col].set_title(df_cols[row][col])
 
     plt.suptitle(main_title)
